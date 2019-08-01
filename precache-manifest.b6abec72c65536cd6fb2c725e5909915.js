e2a12f5b8337e1bb1eba1d483de0b9a2068c99b9
paupaugoz <paugozum1@gmail.com>
2019-08-01 11:45:13 -0700

    Updates

---
+++ precache-manifest.b6abec72c65536cd6fb2c725e5909915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4924fe4ae260e49f456b2821f48ef6e7",
+    "url": "/Yuujin-Site/index.html"
+  },
+  {
+    "revision": "9a524bef0610492f52ce",
+    "url": "/Yuujin-Site/static/css/main.995d51dc.chunk.css"
+  },
+  {
+    "revision": "dc0d2b7a34cc5f09e35b",
+    "url": "/Yuujin-Site/static/js/2.6b0190f2.chunk.js"
+  },
+  {
+    "revision": "9a524bef0610492f52ce",
+    "url": "/Yuujin-Site/static/js/main.d6d24a5e.chunk.js"
+  },
+  {
+    "revision": "fa9439dc35ffbb594985",
+    "url": "/Yuujin-Site/static/js/runtime~main.1e56bcf9.js"
+  }
+]);
